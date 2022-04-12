3a26b650690e2a751e3511116be8d55125a34acb
Pierre-Louis <pierrelouisdeshayes42@gmail.com>
2022-04-12 22:52:45 +0200

    patch ex04

--- module-01/ex04/main.cpp
+++ module-01/ex04/main.cpp
@@ -6,12 +6,15 @@ void ft_replace(std::ifstream &infile, std::ofstream &outfile, std::string s1, s
     std::size_t i = std::string::npos;
     while (getline(infile, buffer))
     {
-        while ((i = buffer.find(s1)) != std::string::npos)
-        {
-            std::cout <<  i << std::endl;
-            buffer.erase(i,s1.length());  
-            buffer.insert(i, s2);
-        }
+        if (s1 == "" || std::string(s1).compare(s2) == 0)    ;
+        else
+            while ((i = buffer.find(s1)) != std::string::npos)
+            {
+                std::cout <<  i << std::endl;
+                buffer.erase(i,s1.length());  
+                buffer.insert(i, s2);
+                i += s2.length();
+            }
         outfile << buffer << std::endl;
     }
     return ;
@@ -29,13 +32,15 @@ int main(int argc, char const *argv[])
     std::string endFileName = argv[1];
     std::ofstream outfile (endFileName + ".replace");
 
-    if (!file || !outfile)
+    if (!file)
     {
         std::cout << "Error : file not found" << std::endl;
+        outfile.close();
         return (1);
     }
+    else
+        ft_replace(file, outfile, argv[2], argv[3]);
 
-    ft_replace(file, outfile, argv[2], argv[3]);
     file.close();
     outfile.close();
     return 0;
